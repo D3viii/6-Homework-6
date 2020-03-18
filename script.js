b02671fffd912aa220cde59f1dd96d1a0e001f70
D3viii <devinsdevs@gmail.com>
2020-03-17 23:36:31 -0400

    did all I could for roght now

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-document.ready(function(){
-    $("#submit-button").click(function(){
+var city = $("#city");
 
-        var city = $("#city").val();
+var apiKey = "cefe799397a3317ff6a38706ef3ba7cc";
 
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
 
-    })
-     
-}))
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+
+    .then(function(response) {
+        console.log(queryURL);
+        console.log(response);
+});
